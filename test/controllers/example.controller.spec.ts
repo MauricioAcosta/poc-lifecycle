982252b47b0b26f8126f75627d5040a5561e7599
MauricioAcosta <amacostapulido@gmail.com>
2023-05-17 07:02:11 -0500

    add test

---
+++ test/controllers/example.controller.spec.ts
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import nock from 'nock';
+import app from '../../src/main';
+
+
+chai.use(chaiHttp);
+chai.should();
+
+const assert = chai.assert;
+const expect = chai.expect;
+
+
+describe('example Controller', () => {
+    beforeEach(() => {
+        nock.cleanAll();
+    });
+    it('Successful Request', (done) => {
+        chai.request(app)
+            .get('/V1/hello/')
+            .set({
+                'Content-Type': 'application/json',
+            }).send()
+            .end((err, res) => {
+                console.log('res_status: ', res.status);
+                console.log('body: ', res.body);
+                console.log('response text:', res.text);
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+});
