fe596ab9880961380520490c83ec187262de1b89
Andres Mauricio Acosta Pulido <amacostapulido@gmail.com>
2023-05-17 17:39:58 -0500

    remove build

--- build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    API_PATH: process.env.API_PATH || '',
-    PORT: process.env.PORT || '3000',
-};
-//# sourceMappingURL=config.js.map
--- build/utilities/example.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ExampleUtility {
-    static async helloWork(UUID) {
-        return `${UUID}: hello world!`;
-    }
-}
-exports.default = ExampleUtility;
-//# sourceMappingURL=example.js.map
