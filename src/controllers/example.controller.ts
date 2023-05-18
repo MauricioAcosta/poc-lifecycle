3183bba6d86c5937e5269246e4842fdee833df6b
Andres Mauricio Acosta Pulido <andres.acosta@davivienda.com>
2023-05-18 15:39:57 -0500

    PL-5 fix bug, code smell and change menssage

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,15 +1,14 @@
 import { Request, Response, Router } from 'express';
 import { uuid } from 'uuidv4';
 import exampleService from '../services/example.service';
-import config from '../config';
 
 const exampleController = Router();
 
 const endpoint = '/';
-exampleController.get(endpoint, async (request: Request, response: Response) => {
+exampleController.get(endpoint, (request: Request, response: Response) => {
   try {
     const UUID = uuid();
-    const message = await exampleService.helloWorld(UUID);
+    const message = exampleService.helloWorld(UUID);
     response.status(200).send({ message });
   } catch (error) {
     response.status(500).send(error);
